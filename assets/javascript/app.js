27a2e6b829a0b0d1f7494b70ee76dc8767147150
Kourtney Reynolds <Kourtney.Reynolds@gmail.com>
2018-03-13 17:25:41 -0700

    RPS set up and html pseudo

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+var config = {
+    apiKey: "AIzaSyC4aihg6-fH1JwzvoaupLHdeTA320Semow",
+    authDomain: "jan-ken-po-3da4a.firebaseapp.com",
+    databaseURL: "https://jan-ken-po-3da4a.firebaseio.com",
+    projectId: "jan-ken-po-3da4a",
+    storageBucket: "jan-ken-po-3da4a.appspot.com",
+    messagingSenderId: "280445464588"
+  };
+  firebase.initializeApp(config);
+
+
+  
