4dceb35d7e3c6f7e6f6413c8ad774146dd499226
Kourtney Reynolds <Kourtney.Reynolds@gmail.com>
2018-03-13 18:26:26 -0700

    add the RPS puesdo code instruction in js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,21 +8,32 @@ var config = {
   };
   firebase.initializeApp(config);
 
+//TEST Connection to Firebase
+//   var database = firebase.database();
 
-  var database = firebase.database();
+//   var count =0;
 
-  var count =0;
+//   $(document).on("click",".count-btn",function(){
+//     count++;
+//     console.log("Clicked it");
+//     //set the reference to the database
+//     database.ref().set({
+//         counter: count
+//     });
+//   })
 
-  $(document).on("click",".count-btn",function(){
-    count++;
-    console.log("Clicked it");
-    //set the reference to the database
-    database.ref().set({
-        counter: count
-    });
-  })
 
-//   $(".count-btn").on("click", function(){
-//    ;
+//This a game to make rock paper scissors
+//Two players login from one form
+    //Will need toggle between player one and player two
 
-//   });
+//Each player can log in
+//Player object will auto create
+//Each player can see whose turn it is
+//Each player will see who wins and looses
+//Each player will NOT see the other persons guess
+//Need to reset
+//When player disconnects the message that they left
+//There is a textbox so that players can talk to each other
+
+
